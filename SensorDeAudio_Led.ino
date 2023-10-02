da7b098c57494d79b736abfad0bd13f058ce90ca
CrazyTroll935 <146011996+CrazyTroll935@users.noreply.github.com>
2023-10-01 22:08:21 -0300

    Add files via upload

---
+++ SensorDeAudio_Led.ino
@@ -0,0 +1,19 @@
+int MIC = 2;
+int LED = 3;
+int VALOR;
+int ESTADO;
+
+void setup() {
+pinMode(MIC, INPUT);
+pinMode(LED, OUTPUT);
+}
+
+void loop() {
+VALOR = digitalRead(MIC);
+if (VALOR == HIGH){
+  ESTADO = digitalRead(LED);
+  digitalWrite(LED, !ESTADO);
+  delay(500);
+}
+
+}
